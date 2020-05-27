1983c9289953bf0fd1738869c9dcc0ab1822faaa
lzlocki <lzlocki@poczta.fm>
2020-05-27 23:23:30 +0200

    Add model views layer and first web

---
+++ LambdaForums/Models/Forum/ForumIndexModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+namespace LambdaForums.Models.Forum
+{
+    public class ForumIndexModel
+    {
+        public IEnumerable<ForumListingModel> ForumList { get; set; }
+    }
+}
---
+++ LambdaForums/Models/Forum/ForumListingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace LambdaForums.Models.Forum
+{
+    public class ForumListingModel
+    {
+        // to jest View Model 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
